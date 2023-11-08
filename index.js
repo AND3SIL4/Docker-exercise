8861184a898e66eb53476dcdf16940fc8639edc6
AND3SIL4-UBUN <afsilva6236@misena.edu.co>
2023-11-08 14:26:47 -0500

    UPDATE : initial structure fixed

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
 import express from "express";
 import mongoose from "mongoose";
 
-// Constante que contiene los atributos de la coleccion en Mongo
-const ANIMAL = mongoose.model('animal', new mongoose.Schema({
+// Constante que contiene los atributos de la colección en MongoDB
+const Animal = mongoose.model('Animal', new mongoose.Schema({
   type: String,
-  state: Strng,
+  state: String,
 }))
 
-// Ejecuta el servidor usando Express
-const APP = express()
+// Ejecuta el servidor Express
+const app = express();
 
-// Conexion con base de datos
-mongoose.connect('')
-
-// Endpoint para listar contenido
-APP.get('/', async (req, res) => {
-  console.log('Preparing...');
-  const ANIMALES = await ANIMAL.rind();
-  return res.send(ANIMALES);
+// Conexión con la base de datos
+mongoose.connect('mongodb://nico:password@monguito:27017/miapp?authSource=admin', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Iniciar el servidor Express después de la conexión exitosa
+    app.listen(3000, () => console.log("I'm listening..."));
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB:', err);
+  });
+
+// Endpoint para obtener todos los animales
+app.get('/', async (req, res) => {
+  console.log('Preparing...');
+  try {
+    const animales = await Animal.find();
+    return res.send(animales);
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).send('Internal Server Error');
+  }
+});
 
 // Endpoint para crear contenido y enviarlo a la base de datos
-APP.get('/crear', async (req, res) => {
+app.get('/crear', async (req, res) => {
   console.log('Creating...');
-  await ANIMAL.create({
-    type: 'Canchitoo...',
-    state: 'Happy'
-  })
-  return res.send('The register has been created...');
-})
-
-// Puerto de escucha de al app
-APP.listen(3000, () => console.log("I'm listening..."));
+  try {
+    await Animal.create({
+      type: 'Canchitoo...',
+      state: 'Happy',
+    });
+    return res.send('The register has been created...');
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).send('Internal Server Error');
+  }
+});
